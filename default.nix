c70773c4499a10213625dd26a694df583219a69c
Hamish Hutchings <moredhel@aoeu.me>
2018-11-28 11:55:04 +0100

    Add packages to repo

--- default.nix
+++ default.nix
@@ -14,9 +14,7 @@
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  illum = pkgs.callPackage ./pkgs/illum { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  kubectx = pkgs.callPackage ./pkgs/kubectx { };
+  haskellPackages.discord-haskell = pkgs.haskellPackages.callPackage ./pkgs/haskellPackages/discord-haskell.nix { };
 }
 
